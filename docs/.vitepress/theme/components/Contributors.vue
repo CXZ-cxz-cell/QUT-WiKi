866e4663146790660b058dfb35fe9bd4087184be
LucasAndrew <lucasandrew0120@outlook.com>
2026-08-03 01:58:21 +0800

    修订: 更新时间显示格式

--- docs/.vitepress/theme/components/Contributors.vue
+++ docs/.vitepress/theme/components/Contributors.vue
@@ -18,7 +18,9 @@ const lastUpdated = computed(() => {
   const y = d.getFullYear()
   const m = String(d.getMonth() + 1).padStart(2, '0')
   const day = String(d.getDate()).padStart(2, '0')
-  return `${y}-${m}-${day}`
+  const h = String(d.getHours()).padStart(2, '0')
+  const min = String(d.getMinutes()).padStart(2, '0')
+  return `${y}-${m}-${day} ${h}:${min}`
 })
 
 function avatarUrl(contributor) {
